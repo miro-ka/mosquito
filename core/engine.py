09a751c5c04bfc38a008e18b9f035c73ba4fe96d
mkarpis <miroslav.karpis@gmail.com>
2017-06-23 00:33:59 +0200

    adding main loop

--- core/engine.py
+++ core/engine.py
@@ -2,7 +2,8 @@ from core.simulation import Simulation
 from core.paper import Paper
 from core.trade import Trade
 import configparser
-import argparse
+import sys
+from termcolor import colored
 
 
 
@@ -20,9 +21,10 @@ class Engine:
         self.config = config_file
         self.strategy = args.strategy
         self.look_back_data = []
+        self.bot = None
         if args.sim:
             self.bot = Simulation(args)
-        elif args.Trade:
+        elif args.trade:
             self.bot = Trade(args)
         elif args.paper:
             self.bot = Paper(args)
@@ -40,19 +42,25 @@ class Engine:
 
 
     def run(self):
+        if self.bot is None:
+            print(colored('The bot type is NOT specified. You need to choose one action (--sim, --paper, --trade)', 'red'))
+            sys.exit()
+
         print('starting simulation')
 
         #TODO: initialize wallet
 
-        #TODO load simulation. trade or paper
-
-        #TODO load strategies
-
         #TODO run loop
-        #while self.sim.finished():
-        #    self.sim.get_next(2)
 
-            #TODO simulation/paper/trade - get next sample(sample_size)
-            #action = strategy.run
-            #TODO Update wallet
-            #TODO report
+        try:
+            while True:
+                self.bot.get_next(self.interval)
+
+                # TODO simulation/paper/trade - get next sample(sample_size)
+                # action = strategy.run
+                # TODO Update wallet
+                # TODO report
+        except KeyboardInterrupt:
+            print('shutting down and writing final statistics!')
+
+
--- green.py
+++ green.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--sim", action='store_true')
     parser.add_argument("--paper", help="Simulate your strategy on real ticker", action='store_true')
-    parser.add_argument("--live", help="REAL trading mode", action='store_true')
+    parser.add_argument("--trade", help="REAL trading mode", action='store_true')
     parser.add_argument("--strategy", help="Name of strategy to be run (if not set, the default one will be used")
 
     args = parser.parse_args()
